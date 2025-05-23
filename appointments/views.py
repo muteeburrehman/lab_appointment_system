fd97a473455fca2afc577b1ba25e97121df541e9
muteeburrehman <muteeb285@gmail.com>
2025-05-23 19:34:16 +0500

    authorization fixtures

--- appointments/views.py
+++ appointments/views.py
@@ -1,7 +1,9 @@
 from rest_framework import viewsets
 from .models import Appointment
 from .serializers import AppointmentSerializer
+from rest_framework import permissions
 
 class AppointmentViewSet(viewsets.ModelViewSet):
     queryset = Appointment.objects.all()
     serializer_class = AppointmentSerializer
+    permission_classes = [permissions.IsAuthenticated]
--- labs/views.py
+++ labs/views.py
@@ -1,11 +1,14 @@
 from rest_framework import viewsets
 from .models import Laboratory, LabTest
 from .serializers import LaboratorySerializer, LabTestSerializer
+from rest_framework import permissions
 
 class LaboratoryViewSet(viewsets.ModelViewSet):
     queryset = Laboratory.objects.all()
     serializer_class = LaboratorySerializer
+    permission_classes = [permissions.IsAuthenticated]
 
 class LabTestViewSet(viewsets.ModelViewSet):
     queryset = LabTest.objects.all()
     serializer_class = LabTestSerializer
+    permission_classes = [permissions.IsAuthenticated]
